f1e9bcdf48927ce8cbfb5986b0adaed4622ca216
Alvix <alvix.wick@gmail.com>
2025-09-14 22:37:21 -0400

    Protected the secret key and debug.

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$xe((pcm)_9ofa%2(b=5ki-*7m0pqz^+f*b9eqi^up!j8eaj)k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
