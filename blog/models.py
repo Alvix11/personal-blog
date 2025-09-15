103dc365bfbf2164de289239baf1cf5fd25936c9
Alvix <alvix.wick@gmail.com>
2025-09-14 21:05:06 -0400

    Renamed model name from Blogs to Blog.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Blogs(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=100)
     publish_date = models.DateField()
     content = models.TextField()
