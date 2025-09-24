1d454b9c684cc8d477c41b4928a2d2c7a4acd5ce
Alvix <alvix.wick@gmail.com>
2025-09-23 23:04:34 -0400

    Added two views: one for updating and another for showing details.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from .models import Blog
 from .forms import BlogForm
@@ -10,8 +10,19 @@ class PostListView(ListView):
     template_name = 'post_list.html'
     context_object_name = 'posts'
     
+class PostDetailView(DetailView):
+    model = Blog
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+    
 class PostCreateView(CreateView):
     model = Blog
     form_class = BlogForm
     template_name = 'post_create.html'
+    success_url = reverse_lazy('list')
+    
+class PostUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'post_update.html'
     success_url = reverse_lazy('list')
