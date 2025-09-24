d29be5b42fa6aac7b9329b2f371cf535998f0914
Alvix <alvix.wick@gmail.com>
2025-09-23 20:06:22 -0400

    Added the URL where the blog will be created.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
 urlpatterns = [
     path('list/', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
 ]
