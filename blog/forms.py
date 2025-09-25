6be490e8475065bb2d6a431e98b5aeac9db34906
Alvix <alvix.wick@gmail.com>
2025-09-24 22:29:26 -0400

    I deleted the date field from the form since in the models field I set it to automatic.

--- blog/forms.py
+++ blog/forms.py
@@ -4,11 +4,6 @@ from .models import Blog
 class BlogForm(forms.ModelForm):
     title = forms.CharField(max_length=100, label='Title', required=True)
     
-    publish_date = forms.DateTimeField(
-        widget=forms.DateInput(attrs={'type': 'date'}),
-        label='Publish Date'
-    )
-    
     content = forms.CharField(
         widget=forms.Textarea(attrs={
             'rows': 10,
@@ -21,4 +16,4 @@ class BlogForm(forms.ModelForm):
     
     class Meta:
         model = Blog
-        fields = ['title', 'publish_date', 'content']
+        fields = ['title', 'content']
