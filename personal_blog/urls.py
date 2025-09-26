bf97bd997c7ddb4f85c7731f3d589f02e5144226
Alvix <alvix.wick@gmail.com>
2025-09-25 21:14:01 -0400

    Added the login URL

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import login_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('blog.urls')),
+    path('login/', login_view, name='login')
 ]
