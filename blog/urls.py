652d17b95adcbf85e4f0ad5cf0b3b726bf2b91df
Alvix <alvix.wick@gmail.com>
2025-09-25 20:29:07 -0400

    Modified the URLs so that they all start with post

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import PostListView, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('list/', PostListView.as_view(), name='list'),
-    path('create/', PostCreateView.as_view(), name='create'),
-    path('detail/<int:pk>/', PostDetailView.as_view(), name='detail'),
-    path('update/<int:pk>/', PostUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', PostDeleteView.as_view(), name='delete')
+    path('post/list/', PostListView.as_view(), name='list'),
+    path('post/create/', PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/detail/', PostDetailView.as_view(), name='detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete')
 ]
