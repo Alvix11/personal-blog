ece2e422727da01e18b04277728e4d72c5b6804d
Alvix <alvix.wick@gmail.com>
2025-09-26 18:57:23 -0400

    Added the logout URL

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import login_view
+from blog.views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('blog.urls')),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
