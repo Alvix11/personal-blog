c23a6d495c2c6c25d105d233b2459dd2c7f3e59f
Alvix <alvix.wick@gmail.com>
2025-09-26 23:40:13 -0400

    Configured 'ALLOWED_HOSTS' and set 'DEBUG' to 'False' as a best practice to prevent vulnerabilities

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
