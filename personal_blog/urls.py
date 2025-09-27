bd14569a534637702e2dbc263f5672cca573dd5e
Alvix <alvix.wick@gmail.com>
2025-09-26 20:54:32 -0400

    Added the home URL

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import login_view, logout_view,signup_view
+from blog.views import login_view, logout_view, signup_view, home
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('blog.urls')),
+    path('', home, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup')
